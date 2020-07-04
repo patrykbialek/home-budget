63cc59023ad76a71a7fe083a8f348fd956bbe02f
Patryk Bialek <p.bialek@kaft.pl>
2020-07-04 13:49:33 +0200

    chore(project):install and configure ngx-translate

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AngularMaterialModule } from './modules/angular-material.module';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [],
@@ -9,9 +10,12 @@ import { AngularMaterialModule } from './modules/angular-material.module';
     CommonModule,
 
     AngularMaterialModule,
+
+    TranslateModule,
   ],
   exports: [
     AngularMaterialModule,
+    TranslateModule,
   ]
 })
 export class SharedModule { }
