5fba7f161b5d0c36c05715ea8b734a139406b982
Patryk Bialek <p.bialek@kaft.pl>
2020-07-06 07:20:32 +0200

    feat(transaction-detail): create transaction-detail component (#4)

--- src/app/transactions/containers/index.ts
+++ src/app/transactions/containers/index.ts
@@ -1,7 +1,10 @@
+import { TransactionDetailComponent} from './transaction-detail/transaction-detail.component';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 
 export const components = [
+  TransactionDetailComponent,
   TransactionListComponent,
 ];
 
+export { TransactionDetailComponent };
 export { TransactionListComponent };
