3a6811c1b6352a5edeb6eddf7a3c5d4058ade527
Patryk Bialek <p.bialek@kaft.pl>
2020-07-29 07:50:05 +0200

    fix(authentication): add utils service (#18)

--- src/app/authentication/services/index.ts
+++ src/app/authentication/services/index.ts
@@ -1,7 +1,10 @@
 import { AuthenticationHttpService } from './authentication-http.service';
+import { AuthenticationUtilsService } from './authentication-utils.service';
 
 export const services = [
   AuthenticationHttpService,
+  AuthenticationUtilsService,
 ];
 
 export { AuthenticationHttpService };
+export { AuthenticationUtilsService };
