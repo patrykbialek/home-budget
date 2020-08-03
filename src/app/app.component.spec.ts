50dc84296e2182aabbe9e205985edb5dda32197d
Patryk Bialek <p.bialek@kaft.pl>
2020-08-03 19:33:20 +0200

    test(app): write unit test for app component (#16)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,70 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AuthenticationFacadeService } from '@authentication/store';
+import { of } from 'rxjs';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  const authStub: any = {
+    authState: {
+      uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2'
+    },
+  };
+
+  let authenticationFacadeServiceStub: AuthenticationFacadeService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase, 'home-budget'),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        EffectsModule.forRoot(),
+        StoreModule.forRoot({}),
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: AngularFireAuth, useValue: authStub },
+        AuthenticationFacadeService,
+        TranslateService,
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'home-budget'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('home-budget');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    authenticationFacadeServiceStub = TestBed.get(AuthenticationFacadeService);
+    authStub.authState = of({ uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2' });
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('home-budget app is running!');
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create the app', () => {
+    const getUserSpy = spyOn(authenticationFacadeServiceStub, 'getUser');
+    component.ngOnInit();
+    expect(getUserSpy).toHaveBeenCalledWith('17WvU2Vj58SnTz8v7EqyYYb0WRc2');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { filter, take, tap } from 'rxjs/operators';
 import { AuthenticationFacadeService } from './authentication/store';
-import { tap, filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'home-budget';
+export class AppComponent implements OnInit {
 
   constructor(
     translate: TranslateService,
-    private fireAuth: AngularFireAuth,
     private authenticationFacadeService: AuthenticationFacadeService,
+    private fireAuth: AngularFireAuth,
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('pl');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('pl');
+  }
 
+  ngOnInit() {
     this.fireAuth
       .authState
       .pipe(
         filter(response => Boolean(response)),
-        // tap(response => console.log('uid', response.uid)),
         tap(response => this.authenticationFacadeService.getUser(response.uid)),
         take(1),
       ).subscribe();
