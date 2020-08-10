52d8ed333f8bf2c8300ca38d4fb2facafe8ea65b
Patryk Bialek <p.bialek@kaft.pl>
2020-08-10 19:40:05 +0200

    test(app-component): refactor tests (#49)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from 'src/environments/environment';
-import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationFacadeService } from '@authentication/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-import { AuthenticationFacadeService } from '@authentication/store';
-import { of } from 'rxjs';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SharedUtilsService } from '@shared/services/shared-utils.service';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { AppComponent } from './app.component';
 
-xdescribe('AppComponent', () => {
+export class AuthenticationFacadeServiceStub {
+  user$: Observable<any>;
+
+  constructor() {
+    this.user$ = of(null);
+  }
+
+  setUser(payload: any) {
+    this.user$ = of(payload);
+  }
+}
+
+export class AngularFireAuthStub {
+  readonly authState: Observable<any>;
+
+  constructor() {
+    this.authState = of({
+      displayName: 'name',
+      email: 'email',
+      uid: 'uid'
+    });
+  }
+};
+
+fdescribe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
-  const authStub: any = {
-    authState: {
-      uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2'
-    },
-  };
-
-  let authenticationFacadeServiceStub: AuthenticationFacadeService;
+  let angularFireAuthStub: AngularFireAuth;
+  let authenticationServiceStub: AuthenticationFacadeService;
+  let sharedUtilsServiceStub: SharedUtilsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -43,8 +65,9 @@ xdescribe('AppComponent', () => {
         AppComponent
       ],
       providers: [
-        { provide: AngularFireAuth, useValue: authStub },
-        AuthenticationFacadeService,
+        { provide: AngularFireAuth, useClass: AngularFireAuthStub },
+        { provide: AuthenticationFacadeService, useClass: AuthenticationFacadeServiceStub },
+        SharedUtilsService,
         TranslateService,
       ]
     }).compileComponents();
@@ -53,8 +76,9 @@ xdescribe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    authenticationFacadeServiceStub = TestBed.get(AuthenticationFacadeService);
-    authStub.authState = of({ uid: '17WvU2Vj58SnTz8v7EqyYYb0WRc2' });
+    angularFireAuthStub = TestBed.get(AngularFireAuth);
+    sharedUtilsServiceStub = TestBed.get(SharedUtilsService);
+    authenticationServiceStub = TestBed.get(AuthenticationFacadeService);
     fixture.detectChanges();
   });
 
@@ -62,10 +86,31 @@ xdescribe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should create the app', () => {
-    const settUserSpy = spyOn(authenticationFacadeServiceStub, 'setUser');
-    // const authStateSpy = spyOn(authStub, 'authState');
+  it(`should set 'user$' when called ngOnInit and 'user' is null`, () => {
+    const result = {
+      displayName: 'name',
+      email: 'email',
+      uid: 'uid'
+    };
+
+    authenticationServiceStub.setUser(null);
     component.ngOnInit();
-    expect(settUserSpy).toHaveBeenCalledWith('17WvU2Vj58SnTz8v7EqyYYb0WRc2');
+    const sub2 = authenticationServiceStub.user$
+      .pipe(
+        tap(response => expect(response).toEqual(result))
+      ).subscribe();
+    sub2.unsubscribe();
+  });
+
+  it(`should call 'setIsMobileSize' method from 'sharedUtilsService' service when called ngOnInit`, () => {
+    const setIsMobileSizeSpy = spyOn(sharedUtilsServiceStub, 'setIsMobileSize');
+    component.ngOnInit();
+    expect(setIsMobileSizeSpy).toHaveBeenCalled();
+  });
+
+  it(`should call 'setIsMobileSize' method from 'sharedUtilsService' service when called onResize`, () => {
+    const setIsMobileSizeSpy = spyOn(sharedUtilsServiceStub, 'setIsMobileSize');
+    component.onResize({ target: { innerWidth: 100 } });
+    expect(setIsMobileSizeSpy).toHaveBeenCalled();
   });
 });
