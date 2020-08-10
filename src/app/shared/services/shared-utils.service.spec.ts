c5e4e891e11509255f8d7500f9468ec37b955f89
Patryk Bialek <p.bialek@kaft.pl>
2020-08-10 18:15:25 +0200

    test(shared-utils): add unit test (#49)

---
+++ src/app/shared/services/shared-utils.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { SharedUtilsService } from './shared-utils.service';
+import { WindowSize } from '@shared/models';
+
+fdescribe('SharedUtilsService', () => {
+  let service: SharedUtilsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SharedUtilsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('when setIsMobileSize', whenSetIsMobileSize);
+});
+
+function whenSetIsMobileSize() {
+  let service: SharedUtilsService;
+
+  beforeEach(inject([],
+    () => {
+      service = new SharedUtilsService();
+    }));
+
+  it(`should call set windowSize value`, () => {
+    service.setIsMobileSize(WindowSize.Desktop);
+
+    service.windowSize$
+      .subscribe(response => {
+        expect(response).toBe('desktop');
+      })
+  });
+}
